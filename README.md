37c1dd6be23b87c29aa105347e935e8451ef71d9
v131v <liya.dobrenkova@ya.ru>
2023-12-08 18:27:59 +0300

    feat: remove mongo

--- README.md
+++ README.md
@@ -1,31 +1,50 @@
 # Stable Diffusion Distributed Network
+
 **Version:** v0.1
 
 **ATTENTION:** *WORK IN PROGRESS*
 
 ## What is it?
+
 Distributed computations network, dedicated to allowing faster image generation speed. It also contains features like a distributed database. This project is created as part of the "Distributed Computations" course at SPbU.
 
-## TODO LIST:
+## Build
 
-### First Priority (minimum required version):
+`docker build -t deps -f ./Dockerfile .`
+`docker build -t stablediffusion ./stablediffusion/.`
+`docker build -t upscaler ./upscalers/.`
+`docker build -t database -f ./database .`
+
+## Usage
+
+`docker run -p 5003:5003 --rm db`
+`docker run --name up1 -v ./:/workspace/cache --env-file ./.env --network host --rm upscaler`
+
+## TODO LIST
+
+### First Priority (minimum required version)
+
 - [x] Image generator (v0.1)
 - [x] Image upscaler (v0.1)
 - [x] Image storage (v0.1)
 - [x] Server (v0.0.1)
 
-### Second priority (optimal version):
+### Second priority (optimal version)
+
 - [ ] Message Broker (To Do)
 - [ ] Logger (To Do)
 - [ ] Log Storage (To Do)
 
-### Third priority (minimum required final version):
+### Third priority (minimum required final version)
+
 - [ ] Load Balancer (To Do)
 
-### Fourth priority (optimal final version):
+### Fourth priority (optimal final version)
+
 - [ ] OpenVPN clusters (To Do)
 - [ ] Config storage (To Do)
 
-## Contributors:
+## Contributors
+
 - **zer0rbt** - idea & code
 - **v113v** - architecture & code (we hope so :^])
--- database/db.py
+++ database/db.py
@@ -1,41 +1,28 @@
-import pymongo
 import os
 
 
 # todo: переименовать коллекцию и название базы данных
 def save_image_to_db(image_bytes: bytes, image_name: str) -> None:
-    image_directory = os.path.abspath("")
+    image_directory = os.getenv("IMAGES_CACHE_PATH")
 
     if not os.path.exists(image_directory):
         os.makedirs(image_directory)
 
     image_path = os.path.join(image_directory, image_name)
 
-    client = pymongo.MongoClient("mongodb://localhost:27017/mydatabase")
-    db = client["mydatabase"]
-    collection = db["1"]
-
-    image_data = {
-        "name": image_name,
-        "path": image_path,
-        "data": image_bytes
-    }
-    collection.insert_one(image_data)
+    file = open(image_path, "wb")
+    file.write(image_bytes)
+    file.close()
 
 
 def get_image_from_db(image_name: str) -> bytes:
-    try:
-        client = pymongo.MongoClient("mongodb://localhost:27017/")
-        db = client["mydatabase"]
-        collection = db["1"]
+    image_path = os.path.join(os.getenv("IMAGES_CACHE_PATH"), image_name)
 
-        image_data = collection.find_one({"name": image_name})
+    if not os.path.exists(image_path):
+        raise ValueError(f"Изображение '{image_name}' не найдено в базе данных.")
 
-        if image_data:
-            image_bytes = image_data.get("data", b"")
-            return image_bytes
-        else:
-            raise ValueError(f"Изображение '{image_name}' не найдено в базе данных.")
+    file = open(image_path)
+    image_data = file.read()
+    file.close()
 
-    except Exception as e:
-        raise ValueError(f"Ошибка при получении изображения из базы данных: {e}")
+    return image_data
